355de07c49a6f8bf39a2b41dd45555066c3b53cb
zigiraplac <zigiraplacide@gmail.com>
2026-07-19 21:00:11 +0200

    feat: Added another function for getting all movies

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,6 +1,7 @@
 import requests
 from config import API_KEY, BASE_URL
-
+import pandas as pd
+import time
 
 def fetch_movie(movie_id):
     res = requests.get(f"{BASE_URL}/movie/{movie_id}", params={"api_key": API_KEY})
@@ -32,3 +33,21 @@ def fetch_movie(movie_id):
     details["crew_size"] = len(credits.get("crew", []))
     details["status_code"] = res.status_code
     return details
+
+
+def fetch_movies(movie_ids):
+
+    movies = []
+
+    for index, movie_id in enumerate(movie_ids, start=1):
+
+        print(f"Fetching {index}/{len(movie_ids)}")
+
+        movie = fetch_movie(movie_id)
+
+        if movie.get("status_code") == 200:
+            movies.append(movie)
+
+        time.sleep(0.25)
+
+    return pd.DataFrame(movies)
